fea7ca9b49d223f72aa89532d9ac9e11d1d132d2
dvpuchades <dapucto@upv.edu.es>
2023-04-13 22:51:16 +0100

    missing model on restaurant was fixed

--- backend/database/models/restaurant.js
+++ backend/database/models/restaurant.js
@@ -1,4 +1,4 @@
-const { Schema } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const restaurantSchema = new Schema({
   brand: { type: Schema.Types.ObjectId, ref: 'Brand', required: true },
