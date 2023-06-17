3943aa33fe34bcc2baf128874b6c3d9483e4decc
dvpuchades <dvpuchades@gmail.com>
2023-06-17 20:18:56 +0000

    fix testing

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const { database } = require('../environment.js');
 
 const DB_URI = database.uri;
-const TESTING_DB_URI = "mongodb://localhost:27017/fattorino-testing"
+const TESTING_DB_URI = "mongodb://127.0.0.1:27017/fattorino-testing"
 
 async function connectToDatabase(testing = false) {
   let mongoUri = DB_URI;
